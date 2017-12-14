ce841b3657b324fb8afbbc75752ea10470ab5ca5
cutewen <549421908@qq.com>
2017-12-14 10:36:25 +0800

    ui and slua

---
+++ Assets/resourceLoader.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class resourceLoader : MonoBehaviour
+{
+	public GameObject m_background;
+	public GameObject m_text;
+	public GameObject m_btn;
+	private Text textcs;
+	private Button btncs;
+	private RectTransform rt;
+	
+	// Use this for initialization
+	void Start()
+	{
+		m_background = new GameObject("background", typeof(Image));
+		m_text = new GameObject("text", typeof(Text));
+		m_btn = new GameObject("button", typeof(Button));
+		init();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void init()
+	{
+		//text
+		m_text.transform.position = new Vector3(Screen.width/ 2.0f, Screen.height/ 3.0f*2,0);
+		
+		textcs = m_text.GetComponent<Text>();
+		textcs.font = Font.CreateDynamicFontFromOSFont("Arial", 1);
+		textcs.fontStyle = FontStyle.Normal;
+		textcs.fontSize = 40;
+
+		rt = m_text.GetComponent<RectTransform>();
+		rt.sizeDelta = new Vector2(230,50);
+		
+		//button
+		m_btn.transform.position = new Vector3(Screen.width/ 2.0f, Screen.height / 3.0f,0);
+
+		btncs = m_btn.GetComponent<Button>();
+		btncs.onClick.AddListener(clickbtn);
+		
+	}
+
+	void clickbtn()
+	{
+		Debug.Log("click the btn");
+	}
+
+}
---
+++ Assets/startManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SLua;
+using UnityEngine.UI;
+
+public class startManager : MonoBehaviour
+{
+	private LuaSvr luaSvr;
+	private LuaTable self;
+
+	public delegate void CreateDelegate(string path, GameObject g);
+
+    static public CreateDelegate createDelegate;
+	
+	// Use this for initialization
+	void Start () {
+		luaSvr = new LuaSvr();
+		luaSvr.init(null, () =>
+		{
+			self = (LuaTable) luaSvr.start("startGame");
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	static public void calledCreate()
+	{
+		if (createDelegate!=null)
+		{
+			createDelegate("GameObject", new GameObject("newObj"));
+		}
+		Debug.Log("calledCreate!");
+	}
+
+	public GameObject createObj(string name, string type)
+	{
+		GameObject newobj;
+
+		switch (type)
+		{
+			case "Image":
+				newobj = new GameObject(name, typeof(Image));
+				break;
+			case "Text":
+				newobj = new GameObject(name, typeof(Text));
+				break;
+			default:
+				newobj = new GameObject(name);
+				break;
+		}
+		//newobj.name = name;
+		Debug.Log("Has create a newobj named "+name);
+		return newobj;
+	}
+}
